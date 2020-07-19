f8ce71ef1c4ea22c6d1a332e2a2c7dd0aba4330d
HBtwozero <53544787+HBtwozero@users.noreply.github.com>
2020-07-18 21:14:18 -0700

    Create app with react create and create homepage and style sass

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./App.css";
+import HomePage from "./Homepage.component";
+import "./homepage.styles.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <HomePage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
