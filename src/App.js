508611cb7a6e3e368acf360aca31c6b4070c42f5
HBtwozero <53544787+HBtwozero@users.noreply.github.com>
2020-07-18 22:07:16 -0700

    second commit
    Broken our homepage into smaller components, finished styling our menu item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 import "./App.css";
-import HomePage from "./Homepage.component";
-import "./homepage.styles.scss";
+import HomePage from "./pages/homepage/homepage.component";
+import "./pages/homepage/homepage.styles.scss";
 
 function App() {
   return (
